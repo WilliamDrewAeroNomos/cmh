297ac144bf508231d69850d7891806389bccae99
William Drew <wdrew@governmentcio.com>
2022-02-10 12:38:07 -0500

    Using variables.tfvars for default values and variables.tf for environment specific values or overrides

---
+++ ahroc/iac/terraform/env-dev/main.tf
@@ -0,0 +1,35 @@
+
+resource "aws_security_group" "instance" {
+  description = "Web server security group"
+  name        = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.SG_FROM_PORT
+    to_port     = var.SG_TO_PORT
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    agency     = "DoD"
+    department = "CMH"
+    project    = "AHROC"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0a8b4cd432b1c3063"
+  instance_type          = var.INSTANCE_TYPE
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    agency     = "DoD"
+    department = "CMH"
+    project    = "AHROC"
+  }
+}
